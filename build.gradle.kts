da2e24492ccba7a05d2b97df57e3cd2974d45258
Fachrudin Effendi <52682756+masudinn@users.noreply.github.com>
2024-12-06 08:19:39 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+implementation(libs.recyclerview)
+implementation (libs.android.glide)
+implementation(libs.androidx.appcompat)
+implementation(libs.material)
+implementation(libs.androidx.activity)
+implementation(libs.androidx.constraintlayout)
+
